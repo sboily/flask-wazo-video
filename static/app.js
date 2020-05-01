0dff93a821430e364ab7fd30b742e0dc475cc070
Emmanuel Quentin <manu.quentin@gmail.com>
2020-05-01 09:53:23 -0400

    Use simplified sdk

--- static/app.js
+++ static/app.js
@@ -6,119 +6,113 @@ var connected = false;
 var room;
 
 var connect = async () => {
-    // get a token from the back end
-    var username = usernameInput.value;
-    if (!username) {
-        alert('Enter your name before connecting');
-        return;
-    }
-    button.disabled = true;
-    button.innerHTML = 'Connecting...';
+  // get a token from the back end
+  var username = usernameInput.value;
+  if (!username) {
+    alert('Enter your name before connecting');
+    return;
+  }
+  button.disabled = true;
+  button.innerHTML = 'Connecting...';
 
-    const data = await fetch('/login', { method: 'POST', body: JSON.stringify({'username': username}) }).then(res => res.json());
-    if (!data) {
-      alert('Could not obtain token. Is the backend running?');
-      button.innerHTML = 'Join call';
-      button.disabled = false;
-    }
-    const phone = new window['@wazo/sdk'].WazoWebRTCClient({
-      host: data.host,
-      displayName: 'My dialer',
-      media: {
-        audio: true,
-      }
-    }, data.session);
-    phone.on('registered', (sipSession) => {
-      if (phone.isRegistered()) {
-        phone.on('accepted', () => {
-          room = _room;
-          room.participants.forEach(participantConnected);
-          room.on('participantConnected', participantConnected);
-          room.on('participantDisconnected', participantDisconnected);
-          connected = true;
-          button.innerHTML = 'Leave call';
-          button.disabled = false;
-          updateParticipantCount();
-        });
-        phone.call(data.room);
-      }
-    });
+  const data = await fetch('/login', { method: 'POST', body: JSON.stringify({ 'username': username }) }).then(res => res.json());
+  if (!data) {
+    alert('Could not obtain token. Is the backend running?');
+    button.innerHTML = 'Join call';
+    button.disabled = false;
+  }
+
+  Wazo.Auth.setHost(data.host);
+  await Wazo.Auth.validateToken(data.session.token);
+
+  room = await Wazo.Room.connect({ extension: '9010', audio: true, video: true, extra: { username } });
+  room.on(room.ON_JOINED, () => {
+    connected = true;
+    updateParticipantCount();
+    button.innerHTML = 'Leave call';
+    button.disabled = false;
+  });
+
+  room.on(room.CONFERENCE_USER_PARTICIPANT_JOINED, participantConnected);
+  room.on(room.CONFERENCE_USER_PARTICIPANT_LEFT, participantDisconnected);
 };
 
 const disconnect = () => {
-    room.disconnect();
-    while (container.childNodes.length > 1)
-        container.removeChild(container.lastChild);
-    button.innerHTML = 'Join call';
-    connected = false;
-    updateParticipantCount();
+  room.disconnect();
+  while (container.childNodes.length > 0) {
+    container.removeChild(container.lastChild);
+  }
+  button.innerHTML = 'Join call';
+  connected = false;
+  updateParticipantCount();
 };
 
 const submitButtonHandler = (event) => {
-    event.preventDefault();
-    if (!connected)
-        connect();
-    else
-        disconnect();
-};
-
-const addParticipantDiv = (sid, name) => {
-    var participant = document.createElement('div');
-    participant.setAttribute('id', sid);
-    participant.setAttribute('class', 'participant');
-
-    var tracks = document.createElement('div');
-    participant.appendChild(tracks);
-
-    var label = document.createElement('div');
-    label.innerHTML = name;
-    participant.appendChild(label);
-
-    container.appendChild(participant);
-    return participant;
+  event.preventDefault();
+  if (!connected) {
+    connect();
+  } else {
+    disconnect();
+  }
 };
 
 const participantConnected = (participant) => {
-    var div = addParticipantDiv(participant.sid, participant.identity);
+  // check if the participant is not already displayed
+  if (document.getElementById(participant.callId)) {
+    return;
+  }
+  var div = addParticipantDiv(participant.callId, participant.extra.username || '...');
 
-    participant.on('trackSubscribed', track => trackSubscribed(div, track));
-    participant.on('trackUnsubscribed', trackUnsubscribed);
+  participant.streams.forEach(stream => {
+    streamSubscribed(div, stream);
+  });
+  participant.on(participant.ON_STREAM_SUBSCRIBED, stream => {
+    streamSubscribed(div, stream);
+  });
 
-    participant.tracks.forEach(publication => {
-        if (publication.isSubscribed)
-            trackSubscribed(div, publication.track);
-    });
+  participant.on(participant.ON_UPDATED, () => {
+    const label = div.querySelector('span');
+    label.innerHTML = participant.extra.username;
+  });
 
-    updateParticipantCount();
+  updateParticipantCount();
+};
+
+const addParticipantDiv = (callId, name) => {
+  var participant = document.createElement('div');
+  participant.setAttribute('id', callId);
+  participant.setAttribute('class', 'participant');
+
+  var video = document.createElement('video');
+  video.style.width = '200px';
+  video.style.height = '200px';
+  participant.appendChild(video);
+
+  var label = document.createElement('span');
+  label.style.display = 'block';
+  label.innerHTML = name;
+  participant.appendChild(label);
+
+  container.appendChild(participant);
+  return participant;
 };
 
 const participantDisconnected = (participant) => {
-    document.getElementById(participant.sid).remove();
-    updateParticipantCount();
+  document.getElementById(participant.callId).remove();
+  updateParticipantCount();
 };
 
-const trackSubscribed = (div, track) => {
-    div.firstChild.appendChild(track.attach());
-};
-
-const trackUnsubscribed = (track) => {
-    track.detach().forEach(element => element.remove());
+const streamSubscribed = (div, wazoStream) => {
+  const video = div.querySelector('video');
+  wazoStream.attach(video);
 };
 
 const updateParticipantCount = () => {
-    if (!connected)
-        count.innerHTML = 'Disconnected.';
-    else
-        count.innerHTML = (room.participants.size + 1) + ' participants online.';
+  if (!connected) {
+    count.innerHTML = 'Disconnected.';
+  } else {
+    count.innerHTML = (room.participants.filter(p => p instanceof Wazo.RemoteParticipant).length + 1) + ' participant(s) online.';
+  }
 };
 
-const addLocalVideo = () => {
-    /*Twilio.Video.createLocalVideoTrack({width: 240}).then(track => {
-        var div = addParticipantDiv('local', 'Me');
-        trackSubscribed(div, track);
-        updateParticipantCount();
-    });*/
-};
-
-addLocalVideo();
 button.addEventListener('click', submitButtonHandler);
